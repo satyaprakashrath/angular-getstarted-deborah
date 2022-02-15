0eab16c39597165902f1c767048227d7b7a24ada
satya <satyaprakash.rath@clarivate.com>
2022-02-15 19:39:17 +0530

    passing paramets to a route

--- src/app/product/product-detail.component.ts
+++ src/app/product/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'pm-product-detail',
@@ -9,9 +10,11 @@ export class ProductDetailComponent implements OnInit {
 
   pageTitle : string = 'Product Detail'
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    Number(this.pageTitle += `: ${id}`);
   }
 
 }
